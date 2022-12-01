d2e2403362900fe267d784c086acc4a3659c0b19
Olyakotenko <115504162+Olyakotenko@users.noreply.github.com>
2022-12-01 06:27:17 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let message="Котенко Ольга";
+alert(message);
+ let a ="6";
+ let b = "3";
+ let c ="7";
+alert(a * b * c)
+
+let i = 0;
+while (i<3){
+  alert(i);
+   i++;
+}
+
+let userName = 'Оля';
+
+function showMessage() {
+   userName = "Оля"; 
+  let message = 'Як справи?';
+  alert(message);
+}
+alert(userName);
+showMessage();
